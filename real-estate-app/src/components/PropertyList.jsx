9f3947d4ec36444dbaca6fefe961f1f29eff06a8
Nishika <nishikasharon@gmail.com>
2026-07-11 12:22:58 +0530

    Create reusable property results list component

---
+++ real-estate-app/src/components/PropertyList.jsx
@@ -0,0 +1,60 @@
+import PropertyCard from "./PropertyCard";
+
+
+function PropertyList({
+properties
+}){
+
+
+if(properties.length===0){
+
+return (
+
+<p>
+No properties found.
+</p>
+
+);
+
+}
+
+
+
+return (
+
+<div className="property-grid">
+
+
+{
+
+properties.map(property =>
+
+
+<PropertyCard
+
+key={
+property.id
+}
+
+property={
+property
+}
+
+/>
+
+
+)
+
+
+}
+
+
+</div>
+
+);
+
+
+}
+
+
+export default PropertyList;
